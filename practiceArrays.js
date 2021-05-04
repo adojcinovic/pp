6c307bc3c6b308e4e2addc2477486345a7b5e620
BIT Student <adojca@gmail.com>
2021-05-04 12:02:56 +0200

    add arrays practice

---
+++ practiceArrays.js
@@ -0,0 +1,23 @@
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+console.log(months[5], months[9], months[0]);
+
+var days = ['Monday', 'Tuesday', 'Wednsday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+console.log(days[6]);
+
+var numbers = [2, -4, 5, -2, -11]
+
+console.log('These are negative numbers:', numbers[1], numbers[3], numbers[4]);
+
+var numbers2 = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+console.log('These numbers are divisible by 3:', numbers2[1], numbers2[4], numbers2[5], numbers2[6], numbers2[8]);
+
+var numbers3 = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]]
+
+console.log(numbers3[0][3]);
+
+console.log(numbers3[3]);
+
+console.log(numbers3[3][1]);

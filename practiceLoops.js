cd35fedc7d181f4dca64181921ec614d432f996c
BIT Student <adojca@gmail.com>
2021-05-04 12:58:09 +0200

    add loops practice

---
+++ practiceLoops.js
@@ -0,0 +1,15 @@
+var punishment = '';
+for (var i = 0; i < 100; i++) {
+    punishment += 'I will never do this again\n';
+}
+
+// console.log(punishment);
+
+var res = '';
+for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < 5; j++) {
+        res += '*\t';
+    }
+    res += '\n';
+}
+console.log(res);

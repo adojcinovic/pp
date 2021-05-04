a81d3bdced3d65b007b6546c3758bbf809a07464
BIT Student <adojca@gmail.com>
2021-05-04 16:40:20 +0200

    add first html project

--- practiceLoops.js
+++ practiceLoops.js
@@ -13,3 +13,23 @@ for (var i = 0; i < 5; i++) {
     res += '\n';
 }
 console.log(res);
+
+//Loops exercises
+
+
+/*1. Write a for loop that will iterate from 0 to 15. For each iteration, it will check if the current
+number is odd or even, and display a message to the screen.*/
+
+for (i = 1; i <= 15; i++) {
+    if (i % 2 === 0) {
+        console.log(i, 'even');
+    } else {
+        console.log(i, 'odd');
+    }
+}
+
+/* 2. Write a program to sum the multiples of 3 and 5 under 1000.*/
+
+for (i = 1; i <= 1000; i++) {
+
+}

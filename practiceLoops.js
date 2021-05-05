540dca89fa20f0d91996365fcf5a293fecb52854
BIT Student <adojca@gmail.com>
2021-05-05 14:37:37 +0200

    add loops practice

--- practiceLoops.js
+++ practiceLoops.js
@@ -81,7 +81,47 @@ for (var i = 1; i <= 20; i++) {
     sumOfSquares += i ** 2
 
 }
-console.log(sumOfSquares);
+// console.log(sumOfSquares);
 
 
 // 7. Write a program that computes average marks of the following students. Then use this average to determine the corresponding grade. 
+
+
+
+
+/* 8.  Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. 
+When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5
+ (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).*/
+
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log(i, 'fizzbuzz')
+    else if (i % 3 === 0) console.log(i, 'fizz');
+    else if (i % 5 === 0) console.log(i, 'buzz');
+    // else console.log(i);
+}
+
+
+// Exercise 2 
+
+/* 1. Write a program that checks if a given element e is in the array a.
+Input:  e = 3, a = [5, -4.2, 3, 7]
+Output: yes*/
+
+var e = 3;
+var a = [5, -4.2, 3, 7];
+var m = []
+for (var i = 0; i < a.length; i++) {
+    if (a[i] === e) console.log('yes');
+}
+
+for (var i = 0; i < a.length; i++) {
+    if (a[i] !== e) {
+        m[i] = a[i]
+    } else if (a[i] === e) {
+        i++
+    }
+
+}
+
+console.log(m);

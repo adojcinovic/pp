3fd339304ad2a7a16ae5380d9f3fb66eaf235c9f
adojcinovic <adojca@gmail.com>
2021-06-02 20:48:32 +0200

    githubsearch

--- ajax/githubsearch.js
+++ ajax/githubsearch.js
@@ -1,6 +1,16 @@
+var input = document.querySelector('input');
+var body = document.querySelector('body');
 var input = document.querySelector('input')
 
+
 function createUsers(users) {
+    users.forEach((element) => {
+        var img = document.createElement('img');
+        var userName = document.createElement('p');
+        img.src = `${element.avatar_url}`
+        userName.textContent = `${element.login}`
+        body.append(img, userName)
+    });
     console.log(users);
 }
 
@@ -9,17 +19,26 @@ function createUsers(users) {
 function getItems() {
     var request = new XMLHttpRequest();
 
-    request.open('GET', `https://api.github.com/search/users?q=pera&per_page=5`);
+    request.open('GET', `https://api.github.com/search/users?q=${input.value}&per_page=5`);
 
 
     request.onload = function () {
-        const data = JSON.parse(request.responseText);
-        createUsers(data.items);
+        const data = JSON.parse(request.responseText).items;
+        createUsers(data);
     }
 
     request.send()
 }
+var flag = false;
 
-getItems()
-createUsers()
-// input.addEventListener('keydown', getData)
+input.addEventListener('keydown', function (e) {
+    if (flag) {
+        location.reload()
+        flag = false
+    }
+    if (e.keyCode === 13) {
+        getItems();
+        input.value = ''
+        flag = true;
+    }
+})
